5558acc290a19f17b3c4b4727e8287388a325d63
mikloskristian <kristian.miklos032@gmail.com>
2022-11-30 16:28:58 +0100

    Added JSON.

---
+++ Scripts/index.js
@@ -0,0 +1,17 @@
+var pitanja = 
+[
+    {
+        tekst: "Koja je najbolja igrica na svijetu?",
+        odgovori: ["GTA San Andreas", "Mafia 2", "RDR2", "Mass Effect 2"],
+        tocanOdgovor: "GTA San Andreas",
+    },
+    {
+        tekst: "Koji je smisao života?",
+        odgovori: ["Biti bogat", "Osnovat obitelj", "Srušit vladu", "Umrjet"],
+        tocanOdgovor: "Umrjet",
+    },
+];
+
+var pitanje = pitanja[0].tekst;
+const kviz = document.getElementById("kviz");
+console.log(kviz); 
